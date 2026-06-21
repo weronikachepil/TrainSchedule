4387b0109790cf122a0f48f7b3d375a83588f899
Veronika Chepil <veronikachepil@Veronikas-Air.netis.cc>
2026-06-21 20:14:31 +0300

    fix: use cors package, explicit railway build command

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,14 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cors = require('cors');
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use((req: any, res: any, next: any) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    if (req.method === 'OPTIONS') { res.sendStatus(200); return; }
-    next();
-  });
+  app.use(cors());
 
   app.useGlobalPipes(
     new ValidationPipe({
